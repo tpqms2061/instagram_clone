71973634c76c66daf2acfa94fa6260fff0a4b123
tpqms2061 <tpqms2061@naver.com>
2025-09-16 19:45:18 +0900

    env config

---
+++ backend/src/main/java/com/ssh/backend/entity/AuthProvider.java
@@ -0,0 +1,6 @@
+package com.ssh.backend.entity;
+
+public enum AuthProvider {
+
+    LOCAL, GOOGLE, GITHUB
+}
---
+++ backend/src/main/java/com/ssh/backend/entity/User.java
@@ -0,0 +1,81 @@
+package com.ssh.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "users", indexes = {
+        @Index(name = "idx_username", columnList = "username"),
+        @Index(name = "idx_email", columnList = "email")
+        //인덱스 : 데이터베이스의 검색속도를 빠르게 하기위한 . 조회속도가 빨라짐
+})
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(name= "full_name") // 데이터베이스에서는 카멜표기법을 안사용해서 Snake표기법으로 _사용
+    private String fullName;
+
+    private String bio;
+
+    @Column(name= "profile_image_url" , columnDefinition = "TEXT")
+    private String profileImageUrl;
+
+    @Enumerated(EnumType.STRING)
+    private AuthProvider authProvider;
+
+    private String ProviderId;
+
+    @CreationTimestamp
+    @Column(name = "create_at" , updatable = false)
+    private LocalDateTime createAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    private boolean enabled;
+
+    @PrePersist
+    protected void onCreate() {
+        enabled = true;
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+}
